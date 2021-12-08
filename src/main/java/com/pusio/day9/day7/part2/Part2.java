570540f6e159cdf9a1e50818d722e084786d6918
Adam Pusio <adam.pusio@allegro.pl>
2021-12-08 14:40:08 +0100

    day9 template

--- src/main/java/com/pusio/day9/day7/part2/Part2.java
+++ src/main/java/com/pusio/day9/day7/part2/Part2.java
@@ -1,13 +1,11 @@
-package com.pusio.day7.part1;
-
-import com.pusio.day7.part2.ProblemSolver;
+package com.pusio.day9.day7.part2;
 
 import java.io.IOException;
 
 /**
  * https://adventofcode.com/2021/day/7
  */
-public class Part1 {
+public class Part2 {
 
     public static void main(String[] args) throws IOException {
         System.out.println("SAMPLE RESULT: " + new ProblemSolver().solve("src/main/resources/day7_sample.input"));
---
+++ src/main/java/com/pusio/day9/day7/part2/ProblemSolver.java
@@ -0,0 +1,10 @@
+package com.pusio.day9.day7.part2;
+
+import lombok.SneakyThrows;
+
+public class ProblemSolver {
+    @SneakyThrows
+    public Long solve(String filePath) {
+        return 0L;
+    }
+}
